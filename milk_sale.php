cb0b5b350c5007e39cfe933f21db2849711ecc52
llaakkkk <lenakirichok@gmail.com>
2017-11-13 21:14:23 +0200

    refactoring

--- milk_sale.php
+++ milk_sale.php
@@ -1,12 +1,12 @@
 <?php
 $sold = 1234;
-$one_litre = 6;
-$salary;
+$oneLitre = 6;
+//$salary;
 if ($sold < 1000) {
-	$salary = $sold*0.2;
-} elseif (($sold >= 1000) || ($sold < 2000)) {
-	$salary = $sold*0.25;
+	$salary = $sold * 0.2;
+} elseif ($sold >= 1000 || $sold < 2000) {
+	$salary = $sold * 0.25;
 } else {
-	$salary = $sold*0.3;
+	$salary = $sold * 0.3;
 }
-echo "Зарплата продавца ".$salary."грн";
+echo 'Зарплата продавца '. $salary . 'грн';
